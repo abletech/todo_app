ffe592618eac8aa84922c3aa5ea45b0758a60c5c
Nigel Ramsay <nigel.ramsay@abletech.co.nz>
2013-07-04 15:15:50 +1200

    Added field validation

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,6 @@
 class Todo < ActiveRecord::Base
   attr_accessible :completed, :order, :title
+
+  validates :title, presence: true
+  validates :order, numericality: {greater_than_or_equal_to: 0}, presence: true, uniqueness: true
 end
