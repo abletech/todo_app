3586ab430f59603c1c3deb27a6cafa5d4680a9e1
Nigel Ramsay <nigel.ramsay@abletech.co.nz>
2013-07-05 11:35:26 +1200

    disable coffeescript

--- config/application.rb
+++ config/application.rb
@@ -54,7 +54,9 @@ module TodoApp
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
+    config.generators.stylesheets = false
+    config.generators.javascripts = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
